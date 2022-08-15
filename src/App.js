31f0b4b967a6b0a6da7ac49b4f50b6e60981ff02
薮木翔大 <yabukishoudai@yabukishoudainoMacBook-Pro.local>
2022-08-15 13:57:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector,useDispatch } from "react-redux";
+import {addPost,deletePost} from "./features/Posts"
+import { useState } from 'react';
+
 
 function App() {
+  const [name,setName] = useState("")
+  const [content,setContent] = useState("")
+
+  const postList = useSelector((state) => state.posts.value);
+  const dispatch = useDispatch()
+  const handlePost = () => {
+    dispatch(addPost({
+      id:postList.length ,
+      name:name,
+      content:content,
+    }))
+    setName("")
+    setContent("")
+  };
+
+  console.log(postList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>掲示板</h1>
+      </div>
+        <div className='addPost'>
+          <input type="text" placeholder="名前" onChange={(e) => setName(e.target.value) }value={name} />
+          <input type="text" placeholder="投稿内容" onChange={(e) => setContent(e.target.value) }value={content} />
+          <button onClick={ () => handlePost()}>投稿</button>
+          <hr/>
+        </div>
+        <div className='displayPosts'>
+          { postList.map((post) => (
+            <div key={post.id} className="post">
+              <h1 className='postName'>{post.name}</h1>
+              <h1 className='postContent'>{post.content}</h1>
+              <button onClick={ () =>dispatch(deletePost({id:post.id}))}>削除</button>
+            </div>
+          )) }
+        </div>
     </div>
   );
 }
---
+++ src/features/Posts.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { PostsData } from "../DammyData";
+
+export const postSlice = createSlice({
+    name: "posts",
+    initialState:{ value:PostsData },
+    reducers: {
+        addPost: (state,action) => {
+            state.value.push(action.payload);
+        },
+        deletePost:(state,action) => {
+            state.value = state.value.filter((post) => post.id !== action.payload.id);
+        },
+    }
+}) 
+
+export const {addPost ,deletePost } = postSlice.actions;
+export default postSlice.reducer;
